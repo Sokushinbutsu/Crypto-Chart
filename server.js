0c77cfd1c5fe2b155d83dfd67f97a697846d4067
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-07-11 23:14:02 -0400

    fixed deployed PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 const axios = require('axios');
 
 app.use(express.static('dist'));
